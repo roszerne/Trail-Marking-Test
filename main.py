2f0ad5dd6b12dd5a4f86758bf583d45ad77f6701
roszerne <ernestrosz00@gmail.com>
2022-09-08 10:58:55 +0200

    Error_messages, saving results, proper drawing, bug fixes

--- main.py
+++ main.py
@@ -22,11 +22,15 @@ def ready_coord(Table, Text, filename, conf):
     for i in range(1, n + 1):
         new_pos = []
         new_pos = points["POINT_" + str(i)]
+        if new_pos[0] > map_size or new_pos[1] > map_size:
+            abort_with_error("Wrong coordinates in the: " + str(filename) + " : " + str(new_pos))
         new_pos[0] = (int)((new_pos[0] / map_size) * SQUARE_SIZE) - offset_x - conf["QUE_RADIUS"]/8
         new_pos[1] = (int)((new_pos[1] / map_size) * SQUARE_SIZE) - offset_y - conf["QUE_RADIUS"]/8
         Table[i - 1].pos = new_pos
         Text[i - 1].pos = new_pos
+            
     
+# Rysowanie kwadratu z ramka na srodku ekranu
 def draw_SQUARE(win):
     global SQUARE_SIZE
     global SCREEN_RES
@@ -47,7 +51,7 @@ def save_beh_results():
         beh_writer.writerows(RESULTS)
     logging.flush()
 
-
+'''
 def show_image(win, file_name, size, key='f7'):
     """
     Show instructions in a form of an image.
@@ -61,7 +65,7 @@ def show_image(win, file_name, size, key='f7'):
         logging.critical(
             'Experiment finished by user! {} pressed.'.format(key[0]))
         exit(0)
-    win.flip()
+    win.flip()'''
 
 
 def read_text_from_file(file_name, insert=''):
@@ -97,7 +101,7 @@ def check_exit(key='f7'):
             'Experiment finished by user! {} pressed.'.format(key))
 
 
-def show_info(win, file_name, insert=''):
+def show_info(win, file_name, mouse, insert=''):
     """
     Clear way to show info message into screen.
     :param win:
@@ -105,15 +109,24 @@ def show_info(win, file_name, insert=''):
     """
     msg = read_text_from_file(file_name, insert=insert)
     msg = visual.TextStim(win, color='white', text=msg,
-                          height=20, wrapWidth=SCREEN_RES['width'])
+                          height=29, wrapWidth=SCREEN_RES['width'])
     msg.draw()
     win.flip()
-    key = event.waitKeys(maxWait = 10, keyList=['f7', 'return', 'space', 'left', 'right'])
+    timer = core.CountdownTimer(10) # czekamy 10 sekund na input od uzytkownika
+    while timer.getTime() > 0:
+        buttons, times = mouse.getPressed(getTime=True)
+        if buttons[0] and times[0] > 0.1:
+            win.flip()
+            mouse.clickReset(buttons = [0])
+            return
+    mouse.clickReset(buttons = [0])
+    win.flip()
+    '''
+    key = event.getKeys(maxWait = 10, keyList=['f7', 'return', 'space', 'left', 'right'])
     if key == ['f7']:
         abort_with_error(
             'Experiment finished by user on info screen! F7 pressed.')
-    win.flip()
-
+    '''
 
 def abort_with_error(err):
     """
@@ -225,51 +238,55 @@ def main():
 
     ready_coord(B_circles, B_texts, "trail\\B_trail.yaml", conf)
 
-    show_info(win, join('.', 'messages', 'hello.txt'))
-    show_info(win, join('.', 'messages', 'guide.txt'))
+    show_info(win, join('.', 'messages', 'hello.txt'), mouse)
+    show_info(win, join('.', 'messages', 'guide.txt'), mouse)
 
     ## TRAILS 
 
     # Part A Training
-    show_info(win, join('.', 'messages', 'before_training_A.txt'))    
+    show_info(win, join('.', 'messages', 'before_training_A.txt'), mouse)    
     core.wait(1)
-    run_trial(win, conf, mouse, A_test_circles, A_test_texts, 8, conf['MAX_TEST_TIME'])
+    run_trial(win, conf, mouse, A_test_circles, A_test_texts, 8, conf['MAX_TEST_TIME'], 'A')
 
     # Part A
-    show_info(win, join('.', 'messages', 'before_test_A.txt'))
+    show_info(win, join('.', 'messages', 'before_test_A.txt'), mouse)
     core.wait(1)
-    t = run_trial(win, conf, mouse, A_circles, A_texts, 25, conf['MAX_A_TIME'])
+    t = run_trial(win, conf, mouse, A_circles, A_texts, 25, conf['MAX_A_TIME'],'A')
     RESULTS.append([PART_ID, 'part_A', t])
 
     # Part B Training
-    show_info(win, join('.', 'messages', 'before_training_B.txt'))    
+    show_info(win, join('.', 'messages', 'before_training_B.txt'), mouse)    
     core.wait(1)
-    run_trial(win, conf, mouse, B_test_circles, B_test_texts, 8, conf['MAX_TEST_TIME'])
+    run_trial(win, conf, mouse, B_test_circles, B_test_texts, 8, conf['MAX_TEST_TIME'], 'B')
 
     # Part B
-    show_info(win, join('.', 'messages', 'before_test_B.txt'))
+    show_info(win, join('.', 'messages', 'before_test_B.txt'), mouse)
     core.wait(1)
-    t = run_trial(win, conf, mouse, B_circles, B_texts, 26, conf['MAX_B_TIME'])
+    t = run_trial(win, conf, mouse, B_circles, B_texts, 26, conf['MAX_B_TIME'], 'B')
     RESULTS.append([PART_ID, 'part_B', t])
 
      # === Cleaning time ===
     save_beh_results()
     logging.flush()
 
-    show_info(win, join('.', 'messages', 'end.txt'))
+    show_info(win, join('.', 'messages', 'end.txt'), mouse)
 
     win.close()
 
 
 
-def run_trial(win, conf, mouse, Circles, Texts, max_num, max_time):
+def run_trial(win, conf, mouse, Circles, Texts, max_num, max_time, part):
 
     global SQUARE_SIZE
+    global RESULTS
+    global RESULTS
     cur_stim = 0 # index of current stimuli
-    #error_mes =  visual.TextStim(win, color='black', text='Naciśnięto blędne koło', 
-    #height=20, pos = [0, 0]) # wiadomosc o bledzie
-    start_mes = visual.TextStim(win, color='white', text='Aby zacząć eksperyment należy kliknij kółko z numerem 1', 
-    height=20, pos = [0, 0 - (SQUARE_SIZE * 1.1)//2]) # wiadomosc powitalna
+    error_mes =  visual.TextStim(win, color='black', text='Naciśnięto blędne koło', 
+    height=27, pos = [0, 0 - (SQUARE_SIZE * 1.2)//2]) # wiadomosc o bledzie
+    start_mes = visual.TextStim(win, color='black', text='Aby zacząć eksperyment należy kliknij kółko z numerem 1', 
+    height=27, pos = [0, 0 - (SQUARE_SIZE * 1.2)//2]) # wiadomosc powitalna
+    mess_square = visual.Rect(win = win, size = [SQUARE_SIZE * 0.8, SQUARE_SIZE * 0.08], fillColor = 'yellow',pos = [0, 0 - (SQUARE_SIZE * 1.2)//2])
+    timer = core.CountdownTimer(0) # timer dla wyswietlania error message
     win.flip()
     # === Start trial ===
     event.clearEvents()
@@ -282,32 +299,42 @@ def run_trial(win, conf, mouse, Circles, Texts, max_num, max_time):
             return 0 # czas rowny 0 oznacza, ze proba nie zostala ukonczona
         draw_SQUARE(win)
         # narysuj bodzce od 0 do cur_stim - 1
-        for i in range(0, cur_stim):
-            # rysowanie lini
-            visual.Line(win = win, start = Circles[i].pos, end = Circles[i + 1].pos, lineWidth = conf['LINE_WIDTH'], lineColor = conf['LINE_COLOR']).draw()
+        for i in range(0, max_num):
+            if i < cur_stim:
+                # rysowanie lini
+                visual.Line(win = win, start = Circles[i].pos, end = Circles[i + 1].pos, lineWidth = conf['LINE_WIDTH'], lineColor = conf['LINE_COLOR']).draw()
             # rysowanie bodzca
             Circles[i].draw()
-            Texts[i].draw()    
-        # narysuj aktualny bodziec     
-        Circles[cur_stim].draw()
-        Texts[cur_stim].draw() 
+            Texts[i].draw()
+        # narysuj wiadomosc powitalna i zresetuj zegar jesli jestesmy na piewszym kole         
         if cur_stim == 0:
+            mess_square.draw()
             start_mes.draw()
+            clock.reset()
+        if timer.getTime() > 0:
+            mess_square.draw()
+            error_mes.draw()
         win.flip()
-        # sprawdz czy nacisnieto poprawne kolko    
-        if mouse.isPressedIn(shape = Circles[cur_stim], buttons = [0]):
-            # zmien kolor ostatniego nacisnietego bodzca
-            Circles[cur_stim].color = conf['STIM_LAST_COLOR']
-            # przywroc orginalny kolor poprzedniemu bodzcowi
-            if cur_stim - 1 >= 0:
-                Circles[cur_stim - 1].color = conf['STIM_COLOR']
-            cur_stim = cur_stim + 1
-            mouse.clickReset(buttons = [0])
+         
+        buttons, times = mouse.getPressed(getTime=True)
+        # sprawdz czy nacisnieto lewy przycisk myszy   
+        if buttons[0]:      
+            if mouse.isPressedIn(shape = Circles[cur_stim], buttons = [0]):
+                # zmien kolor ostatniego nacisnietego bodzca
+                Circles[cur_stim].color = conf['STIM_LAST_COLOR']
+                # przywroc orginalny kolor poprzedniemu bodzcowi
+                if cur_stim - 1 >= 0:
+                    Circles[cur_stim - 1].color = conf['STIM_COLOR']
+                cur_stim = cur_stim + 1
+                # zapisz czas w ktorym nacisnieto kolo
+                RESULTS.append([PART_ID, 'Part_' + part + '_point_' + str(cur_stim), clock.getTime()])
+                # zresetuj czas myszy
+                mouse.clickReset(buttons = [0])
+        # jedno nacisniecie jest rejestrowane wiecej niz jeden raz, dlatego sprawdzamy czy minela przynajmniej jedna sekunda 
+        # od ostatniego klikniecia
+            elif times[0] > 0.3:
+                timer = core.CountdownTimer(3)
                 
-
-                
-
-
     t = clock.getTime()
     return t
 
@@ -316,7 +343,7 @@ def run_trial(win, conf, mouse, Circles, Texts, max_num, max_time):
 if __name__ == '__main__':
     PART_ID=''
     SCREEN_RES = None
-    SQUARE_SIZE = 0.7
+    SQUARE_SIZE = 0.75
     main()
 
 
